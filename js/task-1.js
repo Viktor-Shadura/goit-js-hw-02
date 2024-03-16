17029668900efdef6284bd78b464b1faa6da479d
Viktor Shadura <shaduravik@gmai.com>
2024-03-16 20:18:22 +0200

    PRACTICE 2

--- js/task-1.js
+++ js/task-1.js
@@ -35,3 +35,7 @@
 // Виклик makeTransaction(10, 5000, 8000) повертає "Insufficient funds!"
 // Виклик makeTransaction(8, 2000, 10000) повертає "Insufficient funds!"
 // Виклик makeTransaction(10, 500, 5000) повертає "You ordered 10 droids worth 5000 credits!"
+
+// ____________________________________
+// TODO PRACTICE
+
